44a1d8812017ba7aa28926aab953207a5f54c924
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-11-12 19:44:09 -0500

    radix advance

--- Radix.h
+++ Radix.h
@@ -8,30 +8,19 @@
 
 using namespace std;
 
-const int SIZE = 256; 
-
-class RadixNode;
-
-class RadixTree{
-    RadixNode* root;
-public: 
-    void insert(string n){
-        root = root->insert(n, 0);
-    }
-};
+const int SIZE = 256;
 
 class RadixNode{
     vector <RadixNode*> nodos;
     bool isEnd = false;
     string word;
-    //vector<char> inserted; 
+    //vector<char> inserted;
 
 public:
-    RadixNode(string cname):nodos(SIZE,nullptr), word(cname) {};
+    RadixNode(string &cname):nodos(SIZE,nullptr), word(cname) {};
+    RadixNode(string &cname, bool _isEnd):nodos(SIZE,nullptr), word(cname), isEnd(_isEnd) {};
 
-    
-
-    RadixNode* insert(string word,int pos){    
+    RadixNode* insert(string word,int pos){
         RadixNode * temp = this;
         int idx = pos;
         string common; /*comun*/
@@ -48,6 +37,7 @@ public:
                 string notCommon2 = word.substr(pos + common.size(), (word.size() - common.size());
                 newnode->nodos[notCommon2[0]] = new RadixNode(notCommon2);
                 newnode->nodos[notCommon[0]] = this;
+                newnode->isEnd = true;
                 //newnode->nodos[word[++idx]] = new RadixNode();
                 return newnode; // retornano el padre
             }
@@ -58,6 +48,7 @@ public:
             auto newnode= new RadixNode(common);
             this->word = temp->word.substr(common.size(), (temp->word.size() - common.size());
             newnode->nodos[this->word[0]] = this;
+            newnode->isEnd = true;
             return newnode;
         }
 
@@ -68,11 +59,25 @@ public:
         return this;
     }
 
-    void search(string word){
+    bool search(string &w,int pos){
+        int i;
+        for(i = pos; i < this->word.size() && i < w.size() ; i++){
+            if(w[pos] != this->word[pos])
+                return false;
+        }
 
-    }
+        if(i >= w.size()){
+            if(this->isEnd) return true;
+            else return false;
+        }
 
-    
+        if(this->nodos[w[i]])
+            return this->nodos[w[i]]->search(w,i);
+        else return false;
+
+    };
+
+
     void indexer(){
         cout << "** Indexer **\n";
         //system("find /Users/angelinux/Data -type f -name \"*.mp4\" > list.txt");
@@ -91,17 +96,29 @@ public:
             reverse(filename.begin(), filename.end());
             cout << " Insert filename: " << filename << " Start pos: " << posStartLine << '\n';
             // Insert into Trie Structure
-            this->insert(filename, posStartLine);  
+            this->insert(filename, posStartLine);
             posStartLine = fileList.tellg();
         }
-        
+
         fileList.close();
     }
 
 };
 
-
-
+class RadixTree{
+private:
+    RadixNode* root;
+public:
+    RadixTree(): root{nullptr} {}
+    void insert(string &n){
+        if(!root)
+            root = new RadixNode(n,true);
+        root = root->insert(n, 0);
+    }
+    void search(string n){
+        root->search(n,0);
+    }
+};
 
 
 #endif
